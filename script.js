54ba53e9e0178d98a7fe55b14d94d810f9f9ced5
Sfirtus <pakijuve01@gmail.com>
2023-01-21 15:54:18 +0100

    Texts display in div box and added button animation

--- script.js
+++ script.js
@@ -10,11 +10,7 @@ function getComputerChoice(){
     return choice[random];
 }
 
-
 function playRound(playerSelection, computerSelection){
-
-    console.log(`Computer choice is: ${computerSelection}`);
-    console.log(`Player choice is: ${playerSelection}`);
 
     if(playerSelection === computerSelection)
         return `It's a Tie! You both played ${playerSelection}`
@@ -47,10 +43,6 @@ let roundsCount = 0;
 let playerScore = 0;
 let computerScore = 0;
 
-/*
-TO DO:
- - Make an animation with buttons to highlight what player selected
-*/
 
 //Game starts when player click on a button
 const buttons = document.querySelectorAll('button');
@@ -61,27 +53,48 @@ buttons.forEach((button) => {
             let result = "";
             switch(button.className){
                 case 'Paper':
+                    highlightButton(button.className)
                     playerSelection = 'Paper';
                     result = playRound(playerSelection, computerSelection);
                     displayRoundScore(result);
                     roundsCount++;
                     break;
                 case 'Scissor':
+                    highlightButton(button.className)
                     playerSelection = 'Scissor';
                     result = playRound(playerSelection, computerSelection);
                     displayRoundScore(result);
                     roundsCount++;
                     break;
                 case 'Rock':
+                    highlightButton(button.className)
                     playerSelection = 'Rock';
                     result = playRound(playerSelection, computerSelection);
                     displayRoundScore(result);
                     roundsCount++;
                     break;
             }
+            const image1 = document.querySelector(`.${button.className} img`);
+            image1.addEventListener('transitionend', removeTransition);
             checkNumberRounds(roundsCount);
     });
-});
+}
+);
+
+//Remove the highlight
+function removeTransition(e){
+    if(e.propertyName !== 'transform')
+        return;
+    this.classList.remove('selected');
+}
+
+//Highlight button when clicked
+function highlightButton(buttonClassName){
+    const image = document.querySelector(`.${buttonClassName} img`);
+    image.classList.add('selected');
+    image.addEventListener('transitionend', removeTransition);
+}
+
 
 //Check number of round played
 function checkNumberRounds(roundsCount){
@@ -90,9 +103,7 @@ function checkNumberRounds(roundsCount){
     }
 }
 
-/* TO DO:
-    - Change all console.log into display scores in score-container
-*/
+//Display scores
 function displayRoundScore(result){
 
     if(result.substring(7,10) != "Tie"){
@@ -103,24 +114,79 @@ function displayRoundScore(result){
         }
     }
 
-    console.log(result);
-    console.log(`Computer score: ${computerScore}`);
-    console.log(`Player score: ${playerScore}`);
-    console.log("");
+    const roundWinner = document.querySelector('.round-result');
+    roundWinner.textContent = result;
+
+    const cpuScore = document.querySelector('.computer-score .score-box');
+    cpuScore.textContent = computerScore;
+
+    const plyScore = document.querySelector('.player-score .score-box');
+    plyScore.textContent = playerScore;
     
 }
 
+//Reset scores counters
+function resetScoreCounters() {
+    const cpuScore = document.querySelector('.computer-score .score-box');
+    cpuScore.textContent = '0';
 
-/*                              -------ADD------- 
-When winners is displayed show a pop up to ask if player want to play again and reset
-scores, couters and text in result box 
-*/
+    const plyScore = document.querySelector('.player-score .score-box');
+    plyScore.textContent = '0';
+}
+
+
+
+//Display winner and ask if player wants to play again
 function displayWinner() {
+
+    const body = document.querySelector('.body-container');
+
+    const popUp = document.createElement('div');
+    popUp.classList.add('popup');
+    body.appendChild(popUp);
+
+    const popupDiv = document.querySelector('.popup');
+
+    const popupHeader = document.createElement('div');
+    popupHeader.classList.add('popup-header');
+
     if (playerScore > computerScore) {
-        console.log("Player Wins!");
+        popupHeader.textContent = 'Player Wins!';
     } else if (playerScore < computerScore) {
-        console.log("Computer Wins!");
+        popupHeader.textContent = 'Computer Wins!';
     }
     if (playerScore == computerScore)
-        console.log("It's a Tie!");
+        popupHeader.textContent = 'It\'s a Tie!';
+
+    popupDiv.appendChild(popupHeader);
+
+    const popupBody = document.createElement('div');
+    popupBody.classList.add('popup-body');
+    popupBody.textContent = 'Do you want to play again?';
+    popupDiv.appendChild(popupBody);
+
+    const bodyContainer = document.querySelector('.popup-body');
+
+    const button = document.createElement('button');
+    button.classList.add('play-again-button');
+    button.textContent = 'Play Again';
+    
+
+    const bodyBox = document.querySelector('body');
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+
+    button.addEventListener('click', () => {
+        roundsCount = 0;
+        playerScore = 0;
+        computerScore = 0;
+        resetScoreCounters();
+        body.removeChild(popupDiv);
+        bodyBox.removeChild(overlay);
+    });
+
+    bodyContainer.appendChild(button);
+    bodyBox.appendChild(overlay);
+
 }
